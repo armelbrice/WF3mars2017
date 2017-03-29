b05422566f293c65fb8b63717c4631a66e2ee035
Etudiant <Etudiant>
2017-03-29 17:18:33 +0200

    boucles

---
+++ 29mars/exo9/js/script.js
@@ -0,0 +1,20 @@
+//  Créer un tableau contenant 3 prénoms
+var users = [ 'John', 'Georges', 'Paul', 'Ringo' ];
+
+console.log(users);
+
+
+
+//  Faire une boucle while sur le tableau  pour saluer chacun des prénoms
+//  Définir une variable i à 0
+
+var i = 0;
+
+//  Limiter la boucle à la taille du tableau
+while( i < users.length ){
+
+    console.log( 'Salut ' + users[i] );
+
+    //  incrémenter la variable i
+    i++;
+}
