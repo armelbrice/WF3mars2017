d16a32234b55f56a99eab306a7d07c7c765eaff1
Etudiant <Etudiant>
2017-04-03 12:41:35 +0200

    formulaire

--- 03Avril/exo1/js/script.js
+++ 03Avril/exo1/js/script.js
@@ -5,6 +5,7 @@ $(document).ready( function(){
     //  Vérifier le genre de l'avatar
         var avatarWoman = $('#avatarWoman');
         var avatarMan = $('#avatarMan');
+        var avatarGender;
 
     // => avatarWoman Capter le click
         avatarWoman.click(function(){
@@ -13,7 +14,13 @@ $(document).ready( function(){
             
 
             //  Désactiver avatarMan
-            console.log( avatarMan.prop('checked, false') );
+            avatarMan.prop('checked', false);
+
+            //  Modifier la valeur de avatarGender
+            avatarGender = avatarWoman.val();
+
+            //  Vider le message d'erreur
+            $('.labelGender b').text('');
             
 
         });
@@ -24,9 +31,24 @@ $(document).ready( function(){
             console.log('Je suis un ' + avatarMan.val() );
 
             //  Désactiver avatarWoman
-            console.log( avatarWoman.prop('checked, false') );
+            avatarWoman.prop('checked', false);
+
+            //  Modifier la valeur de avatarGender
+            avatarGender = avatarMan.val();
+
+            //  Vider le message d'erreur
+            $('.labelGender b').text('');
 
         }); 
+
+    //  Supprimer les messages d'erreur
+    $('input, select').focus(function(){
+
+        //  Sélectionner la balise précédent le input
+        $(this).prev().children('b').text('');
+
+
+    });
 
 
     
@@ -37,6 +59,9 @@ $('form').submit(function(evt){
 
     //  Bloquer le comportement par défaut du formulaire
     evt.preventDefault();
+
+    // Définir une variable pour la validation finale du formulaire
+    var formScore = 0;
     
     
 
@@ -54,51 +79,90 @@ $('form').submit(function(evt){
     //  Vérifier les champs du formulaire
         //  => avatarName minimum 5 caractères
         if( avatarName.length < 5 ){
-            console.log('Minimum 5 caractères');
+            
+
+            // Ajouter un message d'erreur dans le label du dessus
+            $('[for="avatarName"] b').text('Minimum 5 caractères');
         
         } else{
             console.log('avatarName OK');
+
+            // Incrémenter la variable formScore
+            formScore++;
         };
 
         // avatarAge entre 1 et 100
         if( avatarAge == 0 || avatarAge > 100 || avatarAge.length == 0 ){
-            console.log('avatarAge entre 1 et 100 ans');
+             // Ajouter un message d'erreur dans le label
+            $('[for="avatarAge"] b').text('Entre 1 et 100 ans');
 
 
         } else{
 
             console.log('avatarAge OK');
+
+            // Incrémenter la variable formScore
+            formScore++;
         };
 
         //  => avatarStyleTop obligatoire
         if(avatarStyleTop == 'null'){
-            console.log('Vous devez choisir un avatarStyleTop');
+            // Ajouter un message d'erreur dans le label du dessus
+            $('[for="avatarStyleTop"] b').text('Choix obligatoire');
 
         } else{
-            console.log('avatarStyleTop OK')
+            
+
+            // Incrémenter la variable formScore
+            formScore++;
         }; 
 
         //  => avatarStyleBottom obligatoire
         if(avatarStyleBottom == 'null'){
-            console.log('Vous devez choisir un avatarStyleBottom');
+              // Ajouter un message d'erreur dans le label du dessus
+            $('[for="avatarStyleBottom"] b').text('Choix obligatoire');
 
         } else{
-            console.log('avatarStyleBottom OK')
+            
+
+            // Incrémenter la variable formScore
+            formScore++;
         }; 
 
+         // => avatarGender vérifier la valeur
+        if( avatarGender == undefined ){
+              // Ajouter un message d'erreur dans .labelGender
+            $('.labelGender b').text('Choix obligatoire');
 
 
-        
+        } else{
+            
 
-        
+            // Incrémenter la variable formScore
+            formScore++;
+
+        };
+
+        // Vérifier la valeur de la variable formScore
+        if( formScore == 5 ){
+            console.log('Le formulaire est validé !');
+
+            // => Envoyer les données du formulaire et attendre la réponse du serveur
+            //  => Le serveur répond true
+
+                //  Vider les champs du formulaire
+                $('form')[0].reset();
+            
 
 
 
-
-});
-
+        };
 
 
+
+    });
+
+       
 
 
 
